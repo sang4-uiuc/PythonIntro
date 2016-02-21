02b349ffbdfa9abc506062ac54e18a0fc5cfed7e
Thomas <sang4@illinois.edu>
2016-02-20 18:22:28 -0600

    deleting some shit

--- Hello.py
+++ Hello.py
@@ -1,3 +1,6 @@
+alright bitches
+yall motherfuckers need jesus
+
 adding something more this is good for the commit 
 dont delete this
 
--- sup.py
+++ sup.py
@@ -1,1 +1,6 @@
 adding some more awesome some bullshit
+
+after that,
+we are gonna test out the merging phase shit
+lets see how this fucking works
+alright? motherfuckers
