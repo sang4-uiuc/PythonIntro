83d3211b39739672c42e9d89b71284845e1a7a50
Thomas Sang <sang4@illinois.edu>
2016-02-23 14:43:18 -0600

    finish week 6 assignment

--- week6one.py
+++ week6one.py
@@ -1,3 +1,21 @@
-just testing the third commit
-just testing the commit system
-just do this fuckin commit
+def computepay(h,r):
+	if h > 0 and h <= 40:
+		total = h * r
+		return total
+	elif h > 40:
+		total = 40 * r + ((h-40) * (1.5*r))
+		return total
+	else:
+		return "Please enter a valid number of hours"
+
+try:
+	hrs = raw_input("Enter Hours:")
+	rate = raw_input("Enter Rate:")
+	hrs = float(hrs)
+	rate = float(rate)
+except:
+	print "Please enter valid numbers!"
+	quit()
+
+p = computepay(hrs,rate)
+print p
